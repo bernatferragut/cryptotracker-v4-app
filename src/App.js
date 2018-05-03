2cd76edd2414366f31bd6125671894a351f66ddf
elbernat <elbernat@gmail.com>
2018-05-03 17:04:47 -0400

    new react syntax
    avoiding to use constructor

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  // 1. Constructor Method + State
-  constructor(){
-    super();
-    this.state = {
-      name: "BERNAT",
-      count:0
-      }
+  // 1. State
+  state = {
+    name: "BERNAT",
+    count:0,
+    addOne : () => {
+      this.setState({ count: this.state.count+1 });
+      console.log('ONE ADDED!!!');
+    }
   }
-  // 2. Extra Methods
-  addOne = () => {
-    this.setState({ count: this.state.count+1 });
-  }
-  // 3. Render Method
+  // 2. Render Method
   render() {
     return (
       <div className="App">
         <h1>GOOD MORNING {this.state.name}</h1>
-        <button onClick={this.addOne}> ADD ONE </button>
+        <button onClick={this.state.addOne}> ADD ONE </button>
         <div>{this.state.count}</div>
       </div>
     );

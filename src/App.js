df7c8ca919bd77bdc971268b13f35e81f3c918a8
elbernat <elbernat@gmail.com>
2018-05-08 21:24:45 -0400

    react and flexbox grid
    react flexbox grid achieved to show the appertements.

--- src/App.js
+++ src/App.js
@@ -21,9 +21,17 @@ class App extends Component {
     };  
 
     return (
-      <div className="App">
-        <Flat flat={flat}/>
-        <Flat flat={flat}/>
+      <div className="app">
+        <div className="main">
+          <div className="search"></div>
+          <div className="flats">
+            <Flat flat={flat}/>
+            <Flat flat={flat}/>
+            <Flat flat={flat}/>         
+            <Flat flat={flat}/>               
+          </div>
+        </div>
+        <div className="map"></div>
       </div>
     );
   }

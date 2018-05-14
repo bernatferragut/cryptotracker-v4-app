08dfd79f41a3372616bf51495e95ab984c482204
elbernat <elbernat@gmail.com>
2018-05-14 12:20:50 -0400

    keep learning react basics

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      text: " 1.this is a state text"
+    }
+  }
+  // setState Method
+  changeState = (e)=> {
+    console.log('changeState has been clicked')
+    this.setState({
+      text: e.target.value
+    })
+  }
 
   render() {
-
-  const title = 'BONJOUR'    
-
     return (
       <div className="app">
-        {title}
+        <input 
+        type="text"
+        onChange={this.changeState}
+        />
+        <h1>{this.state.text}</h1>
       </div>
     )
   }
 }
+
 export default App;

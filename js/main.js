0772a9b0670a498f0dccbf2baa8fea8951daa939
elbernat <elbernat@gmail.com>
2018-05-22 10:44:39 -0400

    Fetching with Vue
    Cryptos properly fetched and very efficient code to add their values provided

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 // JS
 
-console.log('=> file loaded')
+//CoinMarketCap url='https://api.coinmarketcap.com/v2/ticker/?limit=10'
+//CryptoCompare url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,EOS,IOT&tsyms=USD'
 
 const header = new Vue( {
     el:  '#header',
     data: {
-        cryptos:[]
+        cryptos:[],
     },
 
-    // computed: {
-    //     totalUSD() {
-    //         return this.cryptos.reduce((sum, crypto) => {
-    //             return sum + crypto.USD
-    //         },0)
-    //     }
-    // },
-
     created() {
-        const url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,EOS,IOT&tsyms=USD'
+        const url = 'https://api.coinmarketcap.com/v2/ticker/?limit=10'
         fetch(url)
             .then(res => res.json())
             .then(json => {
-                this.cryptos = json
+                this.cryptos = json.data
                 console.log(this.cryptos)
             })
-    }
+            .catch(err => console.log(err))
+    },
+
+    computed: {
+        totalUSD () {
+            return Object.values(this.cryptos).reduce((sum, crypto) => {
+                return (sum + crypto.quotes.USD.price)
+            },0).toFixed(2);
+        }
+    },
 })
-
-
-// try CoinMarketCap url='https://api.coinmarketcap.com/v2/ticker/?limit=10'
 
